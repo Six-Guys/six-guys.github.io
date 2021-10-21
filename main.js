0a747c2a3c7282360f62ea4d4e4984fa3949e442
dxher <dahant17@ecolecatholique.ca>
2021-10-21 10:28:38 -0400

    javascript

---
+++ main.js
@@ -0,0 +1,18 @@
+
+var x = 0;
+function addToCart1(){
+    x += 800;
+    document.getElementById('Price').innerHTML = "Your cart total: " + x +"$";
+}
+function addToCart2(){
+    x += 1200;
+    document.getElementById('Price').innerHTML = "Your cart total: " + x +"$";
+}
+function addToCart3(){
+    x += 1800;
+    document.getElementById('Price').innerHTML = "Your cart total: " + x +"$";
+}
+function addToCart4(){
+    x += 2400;
+    document.getElementById('Price').innerHTML = "Your cart total: " + x +"$";
+}
---
+++ products.js
@@ -0,0 +1,26 @@
+const products = [
+    {
+        id: 1,
+        name: "Eyeroh Basic",
+        price: 800,
+        imgSrc: "images/glasses.jpg",
+    },
+    {
+        id: 2,
+        name: "Eyeroh",
+        price: 1200,
+        imgSrc: "images/glasses.jpg",
+    },
+    {
+        id: 3,
+        name: "Eyeroh Premium",
+        price: 2800,
+        imgSrc: "images/glasses.jpg",
+    },
+    {
+        id: 4,
+        name: "Eyeroh Ultimate",
+        price: 2400,
+        imgSrc: "images/glasses.jpg",
+    }
+]
