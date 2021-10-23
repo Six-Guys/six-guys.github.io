1cc2ec8ca1a4673a9dd5eb7135b79990d6c16fb8
dxher <dahant17@ecolecatholique.ca>
2021-10-23 11:49:59 -0400

    working on cart layout

--- main.js
+++ main.js
@@ -16,3 +16,23 @@ function addToCart4(){
     x += 2400;
     document.getElementById('Price').innerHTML = "Your cart total: " + x +"$";
 }
+function removeFromCart1(){
+    if (x == 0){
+    }
+    else {
+    x -= 800;
+    document.getElementById('Price').innerHTML = "Your cart total: " + x +"$";
+    }
+}
+function removeFromCart2(){
+    x -= 1200;
+    document.getElementById('Price').innerHTML = "Your cart total: " + x +"$";
+}
+function removeFromCart3(){
+    x -= 1800;
+    document.getElementById('Price').innerHTML = "Your cart total: " + x +"$";
+}
+function removeFromCart4(){
+    x -= 2400;
+    document.getElementById('Price').innerHTML = "Your cart total: " + x +"$";
+}
